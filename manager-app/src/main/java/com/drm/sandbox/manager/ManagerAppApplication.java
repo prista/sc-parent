8fa4f8c4410bf4b64c640f93162f954c447bb75a
prista <drmhere@icloud.com>
2026-07-20 17:57:36 +0300

    #part 1 - final commit

--- manager-app/src/main/java/com/drm/sandbox/manager/ManagerAppApplication.java
+++ manager-app/src/main/java/com/drm/sandbox/manager/ManagerAppApplication.java
@@ -2,8 +2,10 @@ package com.drm.sandbox.manager_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.drm.sandbox")
 public class ManagerAppApplication {
 
 	public static void main(String[] args) {
---
+++ manager-app/src/main/java/com/drm/sandbox/manager/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.drm.sandbox.manager.repository;
+
+
+import com.drm.sandbox.manager.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+
+    List<Product> findAll();
+
+    Product save(Product product);
+
+    Optional<Product> findById(Integer productId);
+
+    void deleteById(Integer id);
+}
