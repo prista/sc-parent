ea1803641831b3ecc2fda6e98b0c2b5b76f3e71f
prista <drmhere@icloud.com>
2026-09-10 19:43:39 +0300

    #createProduct integration test

--- catalogue-service/src/test/java/com/drm/sandbox/catalogue/controller/ProductsRestControllerIT.java
+++ catalogue-service/src/test/java/com/drm/sandbox/catalogue/controller/ProductsRestControllerIT.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
@@ -12,8 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 // Rolls back the transaction after each test, so data changes are reverted and don't leak between tests
 @Transactional
@@ -53,5 +53,30 @@ class ProductsRestControllerIT {
                                 ]
                                 """)
                 );
+    }
+
+    @Test
+    void createProduct_RequestIsValid_ReturnsNewProduct() throws Exception {
+        // given
+        var requestBuilder = MockMvcRequestBuilders.post("/catalogue-api/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"title": "One more product", "details": "Some details about new product"}""")
+                .with(jwt().jwt(builder -> builder.claim("scope", "edit_catalogue")));
+
+        // when
+        this.mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isCreated(),
+                        header().string(HttpHeaders.LOCATION, "http://localhost/catalogue-api/products/1"),
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON),
+                        content().json("""
+                                {
+                                    "id": 1,
+                                    "title": "One more product",
+                                    "details": "Some details about new product"
+                                }"""));
     }
 }
