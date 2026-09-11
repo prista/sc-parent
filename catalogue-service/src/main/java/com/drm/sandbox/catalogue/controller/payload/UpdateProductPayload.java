1ac6643951b11281586b9477c0735e7617bbf32c
prista <drmhere@icloud.com>
2026-09-11 09:26:29 +0300

    #integration test (using TC db)

--- catalogue-service/src/main/java/com/drm/sandbox/catalogue/controller/payload/UpdateProductPayload.java
+++ catalogue-service/src/main/java/com/drm/sandbox/catalogue/controller/payload/UpdateProductPayload.java
@@ -1,10 +1,10 @@
 package com.drm.sandbox.catalogue.controller.payload;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UpdateProductPayload(
-        @NotNull(message = "{catalogue.products.update.errors.title_is_null}")
+        @NotBlank(message = "{catalogue.products.update.errors.title_is_null}")
         @Size(min = 3, max = 50, message = "{catalogue.products.update.errors.title_size_is_invalid}")
         String title,
         @Size(max = 1000, message = "{catalogue.products.update.errors.details_size_is_invalid}")
--- catalogue-service/src/test/java/com/drm/sandbox/catalogue/controller/ProductsRestControllerIT.java
+++ catalogue-service/src/test/java/com/drm/sandbox/catalogue/controller/ProductsRestControllerIT.java
@@ -11,6 +11,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Locale;
+
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -56,6 +58,23 @@ class ProductsRestControllerIT {
     }
 
     @Test
+    @Sql("/sql/products.sql")
+    void findProducts_UserIsNotAuthorized_ReturnsForbidden() throws Exception {
+        // given
+        var requestBuilder = MockMvcRequestBuilders.get("/catalogue-api/products")
+                .param("filter", "Product")
+                .with(jwt()); // valid jwt with default claims, does not contain SCOPE_view_catalogue
+
+        // when
+        this.mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isForbidden()
+                );
+    }
+
+    @Test
     void createProduct_RequestIsValid_ReturnsNewProduct() throws Exception {
         // given
         var requestBuilder = MockMvcRequestBuilders.post("/catalogue-api/products")
@@ -78,5 +97,51 @@ class ProductsRestControllerIT {
                                     "title": "One more product",
                                     "details": "Some details about new product"
                                 }"""));
+    }
+
+    @Test
+    void createProduct_RequestIsInvalid_ReturnsProblemDetail() throws Exception {
+        // given
+        var requestBuilder = MockMvcRequestBuilders.post("/catalogue-api/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"title": "  ", "details": null}""")
+                .locale(Locale.of("ru", "RU"))
+                .with(jwt().jwt(builder -> builder.claim("scope", "edit_catalogue")));
+
+        // when
+        this.mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_PROBLEM_JSON),
+                        content().json("""
+                                {
+                                    "errors": [
+                                        "Название товара должно быть указано",
+                                        "Название товара должно быть от 3 до 50 символов"
+                                    ]
+                                }"""));
+    }
+
+    @Test
+    void createProduct_UserIsNotAuthorized_ReturnsForbidden() throws Exception {
+        // given
+        var requestBuilder = MockMvcRequestBuilders.post("/catalogue-api/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"title": "  ", "details": null}""")
+                .locale(Locale.of("ru", "RU"))
+                // user has only view_catalogue scope, but edit_catalogue is required to create a product
+                .with(jwt().jwt(builder -> builder.claim("scope", "view_catalogue")));
+
+        // when
+        this.mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isForbidden()
+                );
     }
 }
